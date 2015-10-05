c592f2a6280849c6139f1bfe838a8e41bdf06bbf
Matthew Spence <msaspence@gmail.com>
2015-10-05 17:46:52 +0100

    Update a few test to updated rspec syntax

--- spec/unit/activated_ui/ui_activated_spec.rb
+++ spec/unit/activated_ui/ui_activated_spec.rb
@@ -11,27 +11,27 @@ describe ActivatedUI do
     end
 
     it "returns true if the provided key is present in #stored_activated_ui" do
-      ui_activated?(:a).should be_true
+      ui_activated?(:a).should be true
     end
 
     it "returns false if the provided keys isn't present in #stored_activated_ui" do
-      ui_activated?(:d).should be_false
+      ui_activated?(:d).should be false
     end
 
     it "returns false if none of the provided keys are present in #stored_activated_ui" do
-      ui_activated?(:d, :e).should be_false
+      ui_activated?(:d, :e).should be false
     end
 
     it "returns false if not all of the provided keys are present in #stored_activated_ui" do
-      ui_activated?(:a, :d).should be_false
+      ui_activated?(:a, :d).should be false
     end
 
     it "returns true if the provided key is present in #stored_activated_ui" do
-      ui_activated?(:a).should be_true
+      ui_activated?(:a).should be true
     end
 
     it "returns true if all the provided key are present in #stored_activated_ui" do
-      ui_activated?(:a, :b).should be_true
+      ui_activated?(:a, :b).should be true
     end
 
   end
